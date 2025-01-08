af87e0fcea66d1360557586fb6a5f3e4f8e68f5b
cnuneze <23690843+cnuneze@users.noreply.github.com>
2025-01-08 17:56:16 -0300

    Fix: Adjust quantity of divs created

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 function init()
 {    
     let gridSize = 16;
+    let itemQuantity = gridSize * gridSize;
 
     const container = document.querySelector('#container');
     
-    for (let i = 0; i < gridSize; i++) {
+    for (let i = 0; i < itemQuantity; i++) {
         const item = document.createElement('div');
         item.classList.add('grid-item');
         
