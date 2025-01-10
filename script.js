2576305e7f9fa94d45d65332fc6310e1094b70be
cnuneze <23690843+cnuneze@users.noreply.github.com>
2025-01-10 18:20:16 -0300

    Fix: Add grid redraw with new dimensions
    Min width and heigth properties has been removed from
    styles.css, this will be calculated every time the grid
    has drawed

--- script.js
+++ script.js
@@ -1,21 +1,12 @@
-function init()
-{    
-    let gridSize = 16;
-    let itemQuantity = gridSize * gridSize;
-
-    const container = document.querySelector('#container');
-    
-    for (let i = 0; i < itemQuantity; i++) {
-        const item = document.createElement('div');
-        item.classList.add('grid-item');
-        
-        container.appendChild(item);
-    }
-
-    applyHoverTrailEffect();
+function start()
+{
+    let gridInitialSize = 16;
+    startEvents();
+    drawGrid(gridInitialSize);
 }
 
-function applyHoverTrailEffect() {
+function applyHoverTrailEffect()
+{
     const grid = document.querySelectorAll('.grid-item');
     grid.forEach(item => {
         item.addEventListener('mouseenter', (event) => {
@@ -30,4 +21,55 @@ function applyHoverTrailEffect() {
     });
 }
 
-init();
+function drawGrid(size)
+{
+    gridItemDimension = getItemDimension(size) + 'px';
+    let itemQuantity = size * size;
+
+    const container = document.querySelector('#container');
+    
+    for (let i = 0; i < itemQuantity; i++) {
+        const item = document.createElement('div');
+        item.classList.add('grid-item');
+        item.style['min-width'] = gridItemDimension;
+        item.style['min-height'] = gridItemDimension;
+        
+        container.appendChild(item);
+    }
+
+    applyHoverTrailEffect();
+}
+
+function getItemDimension(size)
+{
+    const gridDimensionsPx = 1200;
+    return gridDimensionsPx / size;
+}
+
+function destroyGrid()
+{
+    const gridItems = document.querySelectorAll('.grid-item');
+    gridItems.forEach((item) => 
+    {
+        item.parentNode.removeChild(item);
+    });
+}
+
+function startEvents()
+{
+    const gridSizeButton = document.querySelector('#btn_gridSize');
+
+    gridSizeButton.addEventListener('click', (event) =>
+    {
+        let newGridSize = prompt('Ingrese tamaño del grid:', 16);
+
+        console.log(newGridSize);
+        
+        if (newGridSize) {
+            destroyGrid();
+            drawGrid(newGridSize, true);
+        }
+    });
+}
+
+start();
